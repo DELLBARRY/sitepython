cd4eb9273794ed218b2602b5ebffabfd01723ee9
Hamady <hamousma2@yahoo.fr>
2017-12-16 20:00:13 +0100

    modif

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'm+^00_3x9es7)b5=$e8#$kavmm3upjo#-ia$w$$i#&11h7lxg+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ['127.0.0.1', 'DELLBARRY.pythonanywhere.com']
 
 
@@ -99,6 +100,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
